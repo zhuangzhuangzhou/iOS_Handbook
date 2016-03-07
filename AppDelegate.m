0f65dc17d3b30a51dea42e25fd7637eb991e391c
zhuangzhuang <zhuangzhuangzhou@live.cn>
2016-03-07 17:20:17 +0800

    add

--- AppDelegate.m
+++ AppDelegate.m
@@ -2,6 +2,8 @@
 #define KScreenHeight (CGRectGetHeight([UIScreen mainScreen].bounds)) //屏幕高度
 #define appDelegate ((AppDelegate *)([UIApplication sharedApplication].delegate))
 
+UIKIT_EXTERN//简单来说，就是将函数修饰为兼容以往C编译方式的、具有extern属性(文件外可见性)、public修饰的方法或变量库外仍可见的属性
+
 #pragma mark UIWindow
 
     /*
